d3e6b034a27d22d59269939c8f12568191d0f102
TravisFahey <88859681+TravisFahey@users.noreply.github.com>
2024-03-08 19:10:54 -0500

    Home page edits

--- RepiBuddy/Controllers/HomeController.cs
+++ RepiBuddy/Controllers/HomeController.cs
@@ -6,26 +6,30 @@ namespace RepiBuddy.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+	private readonly ILogger<HomeController> _logger;
+    private readonly IWebHostEnvironment _hostEnvironment;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostEnvironment)
+	{
+		_logger = logger;
+		_hostEnvironment = hostEnvironment;
+	}
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	public IActionResult Index()
+	{
+		return View();
+	}
+	
+	public IActionResult GetImage(string filename)
+		{
+			var path = Path.Combine(_hostEnvironment.WebRootPath, "img", filename);
+			var imageFileStream = System.IO.File.OpenRead(path);
+			return File(imageFileStream, "image/jpeg");
+		}
+	
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
