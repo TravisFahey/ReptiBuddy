1ab5815337ef92f02d1bfbaa5381e6388ff8db53
Wesley Jump <wesleyt2002@icloud.com>
2025-06-09 19:34:06 -0400

    added status code

--- ReptiBuddy/Models/ErrorViewModel.cs
+++ ReptiBuddy/Models/ErrorViewModel.cs
@@ -2,7 +2,8 @@ namespace ReptiBuddy.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string? RequestId { get; set; } 
+    public string? StatusCode { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
